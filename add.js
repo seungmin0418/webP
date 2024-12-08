6b47ea4e699e398cb6a7e533b8470abbc7fccdcc
seungmin0418 <dltmdals041824@gmail.com>
2024-12-08 19:47:00 +0900

    Delete add.js

--- add.js
+++
@@ -1,8 +0,0 @@
-onmessage=function(e){
-    let sum=0;
-    let from=parseInt(e.data.from);
-    let to=parseInt(e.data.to);
-    for(let i=from; i<=to;i++)
-        sum+=i;
-    this.postMessage(sum); // 곱하기 할때는 let sum을 지우고 postmessage ()에 from*to를 넣으면됨
-}
